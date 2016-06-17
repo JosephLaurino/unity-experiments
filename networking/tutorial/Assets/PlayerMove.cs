ef72f5f8522a65a747df18de942dccaeb04f228b
Joseph Laurino <joseph.laurino@gmail.com>
2016-06-16 21:26:38 -0700

    networking tutorial
    following along the tutorial here…
    http://docs.unity3d.com/Manual/UNetSetup.html

---
+++ networking/tutorial/Assets/PlayerMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.Networking;
+
+public class PlayerMove : NetworkBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+        if (!isLocalPlayer)
+            return;
+        
+        var x = Input.GetAxis("Horizontal")*0.1f;
+        var z = Input.GetAxis("Vertical")*0.1f;
+
+        transform.Translate(x, 0, z);
+	}
+
+    public override void OnStartLocalPlayer()
+    {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+    }
+}
