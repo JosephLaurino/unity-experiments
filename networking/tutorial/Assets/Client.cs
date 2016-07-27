13d76f28f4bef8fcc819e10236bdd1f686bc1f3d
Joseph Laurino <joseph.laurino@gmail.com>
2016-07-27 00:11:08 -0700

    added a client scene that will automatically connect to a host

---
+++ networking/tutorial/Assets/Client.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Client : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        UnityEngine.Networking.NetworkManager networkManager = GetComponent<UnityEngine.Networking.NetworkManager>();
+        networkManager.StartClient();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
